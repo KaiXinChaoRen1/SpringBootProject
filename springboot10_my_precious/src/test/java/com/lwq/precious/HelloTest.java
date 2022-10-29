5e71b729d617b30d21a0ae0c5ea927aacb3198a7
lwqq <hehe@hehe.com>
2022-10-29 14:29:16 +0800

    java知识点练习

---
+++ springboot10_my_precious/src/test/java/com/lwq/precious/HelloTest.java
@@ -0,0 +1,14 @@
+package com.lwq.precious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTest {
+
+    @Test
+    void name1() {
+        System.out.println("************************************************************");
+    }
+
+}
