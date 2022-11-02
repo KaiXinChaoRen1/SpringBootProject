da37edb057f6dad3c7fc308550950e1f1b3edfb5
lwq <2065277594@qq.com>
2022-11-02 21:12:13 +0800

    LocalDateTime

--- springboot10_my_precious/src/test/java/com/lwq/precious/p04_LocalDateTime/LocalDateTimeTest.java
+++ springboot10_my_precious/src/test/java/com/lwq/precious/p04_LocalDateTime/LocalDateTimeTest.java
@@ -13,29 +13,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class LocalDateTimeTest {
-
+    /**
+     * 获取LocalDate
+     */
     @Test
     public void name() {
         //获取当前时间或日期
-        System.out.println(LocalDate.now());
-        System.out.println(LocalDateTime.now());
+        System.out.println("1==>" + LocalDate.now());
+        System.out.println("2==>" + LocalDateTime.now());
 
         //根据年月日指定日期
-        LocalDate localDate1 = LocalDate.of(2019, 2, 28);// 年月日
-        System.err.println(localDate1);
+        System.err.println("3==>" + LocalDate.of(2019, 2, 28));
 
-        
-        String str = "2019-02-31";// 日期31号之后直接报错,31之前,如果大于当月最大,那就是当月最大那一号
-        LocalDate localDate = LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        System.err.println(localDate);
+        //根据字符串获取
+        String dateStr = "2019-02-31";// 日期31号之后直接报错,31之前,如果大于当月最大,那就是当月最大那一号
+        System.err.println("4==>" + LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
         String dateTimeStr = "2018-07-28 14:11:15";
-        LocalDateTime localDateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        System.err.println(localDateTime);
+        System.err.println("5==>" + LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 
+
     /**
-     * 相差多少天
+     * 相差时间,多少天(当然也能看相差的,年,月等,去查API)
      */
     @Test
     public void name2() {
@@ -51,45 +51,53 @@ public class LocalDateTimeTest {
     @Test
     public void name3() {
         // LoaclDate转换Date
-        LocalDate localDate = LocalDate.of(2019, 2, 8);
-        Date day = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        System.out.println(day);
+        LocalDate localDate = LocalDate.now();
+        System.out.println(Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+
         // LoaclDateTime转换Date
         LocalDateTime localDateTime = LocalDateTime.now();
-        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-        System.out.println(date);
+        System.out.println(Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()));
 
     }
 
+    /**
+     *  Date转LocalDateTime
+     *  Date转LocalDate
+     */
     @Test
     public void name33() {
-        // Date转LocalDateTime(LocalDate)
+
         Date date = new Date();
-        LocalDateTime localDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        System.out.println(localDateTime);
-        System.out.println(localDate);
+        System.out.println("LocalDateTime==>" + date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+        System.out.println("LocalDate======>" + date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
     }
 
+    /**
+     * 转换为字符串
+     */
     @Test
     public void name4() {
-        // 时间转字符串格式化
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss?SSS");
-        String dateTime = LocalDateTime.now(ZoneOffset.of("+8")).format(formatter);
-        System.out.println(dateTime);
+        String strDateTime = LocalDateTime.now(ZoneOffset.of("+8")).format(formatter);
+        System.out.println(strDateTime);
 
     }
 
+    /**
+     * 符串转时间
+     */
     @Test
     public void name5() {
-        // 字符串转时间
         String dateTimeStr = "2018-07-28 14:11:15";
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, df);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTimeStr, dateTimeFormatter);
+        System.out.println(localDateTime);
 
     }
 
-    // 转换时间戳
+    /**
+     * 转换为时间戳
+     */
     @Test
     public void name6() {
         LocalDateTime localDateTime = LocalDateTime.now();
@@ -97,13 +105,16 @@ public class LocalDateTimeTest {
         System.out.println(timestamp);
     }
 
-    //时间戳转换
+    /**
+     *     时间戳转换LocalDate & LocalDateTime
+     */
     @Test
     public void name7() {
         long timestamp = System.currentTimeMillis();
         LocalDate localDate = Instant.ofEpochMilli(timestamp).atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDateTime localDateTime = Instant.ofEpochMilli(timestamp).atZone(ZoneId.systemDefault()).toLocalDateTime();
-
+        System.out.println(localDate);
+        System.out.println(localDateTime);
     }
 
 }
