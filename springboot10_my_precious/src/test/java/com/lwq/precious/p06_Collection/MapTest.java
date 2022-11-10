2120907556c684b63acf0eca4d9f79bc916ec173
lwqq <hehe@hehe.com>
2022-11-10 13:37:33 +0800

    map的一些操作

--- springboot10_my_precious/src/test/java/com/lwq/precious/p06_Collection/MapTest.java
+++ springboot10_my_precious/src/test/java/com/lwq/precious/p06_Collection/MapTest.java
@@ -23,4 +23,30 @@ public class MapTest {
         System.out.println(values);
         System.out.println(values.getClass());
     }
+
+    @Test
+    public void name2() {
+        HashMap<String, String> myMap = new HashMap<String,String>();
+        myMap.put("1", "hehe");
+        myMap.put("2", "haha");
+        myMap.put("3", "xixi");
+
+        myMap.replace("3", "--","雷霆半月斩");
+        System.out.println(myMap);
+
+        myMap.replace("3", "闪电旋风劈");
+        System.out.println(myMap);
+
+      
+        myMap.remove("1");
+        System.out.println(myMap);
+
+        myMap.remove("2","??");
+        System.out.println(myMap);
+
+        myMap.remove("2","haha");
+        System.out.println(myMap);
+
+
+    }
 }
