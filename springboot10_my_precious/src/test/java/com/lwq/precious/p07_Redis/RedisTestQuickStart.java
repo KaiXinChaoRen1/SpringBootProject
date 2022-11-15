8ed8205e74630e4406d2967534cd7a67c4e07387
lwq <2065277594@qq.com>
2022-11-15 19:01:37 +0800

    redisTest

---
+++ springboot10_my_precious/src/test/java/com/lwq/precious/p07_Redis/RedisTestQuickStart.java
@@ -0,0 +1,14 @@
+package com.lwq.precious.p07_Redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class RedisTestQuickStart {
+
+    @Test
+    public void name1(){
+
+    }
+
+}
