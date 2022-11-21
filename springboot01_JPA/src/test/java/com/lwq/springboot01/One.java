f6e195bb47904b3f99c77003af16f7520f17e61e
lwq <2065277594@qq.com>
2022-11-21 15:28:19 +0800

    1

--- springboot01_JPA/src/test/java/com/lwq/springboot01/One.java
+++ springboot01_JPA/src/test/java/com/lwq/springboot01/One.java
@@ -2,10 +2,10 @@ package com.lwq.springboot01;
 
 import com.lwq.springboot01.Entity.schoolstory.Person;
 import com.lwq.springboot01.dao.schoolRepository.PersonRepository;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -13,6 +13,25 @@ public class One {
     @Autowired
     private PersonRepository pr;
 
+    @Commit
+    @Transactional
+    @Test
+    public void name6(){
+        Person p1 = Person.builder().name("lwq").age(55).build();
+        pr.save(p1);
+        System.out.println(p1);     //数据库的修改会同步到内存中的java对象
+        p1.setName("www");          //session内java对象修改会同步到数据库
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void name5(){
+        Person p1 = Person.builder().id(1).name("lwq").age(55).build();
+        pr.save(p1);
+        System.out.println(p1);     //数据库的修改会同步到内存中的java对象
+        p1.setName("www");          //session内java对象修改会同步到数据库(手动加的id保存的对象就不会同步)
+    }
 
 
     @Test
