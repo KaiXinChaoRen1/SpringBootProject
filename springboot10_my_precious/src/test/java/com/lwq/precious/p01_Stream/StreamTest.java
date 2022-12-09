c41f754f1588495af62ea4539a498c55a705d813
lwqq <hehe@hehe.com>
2022-12-09 19:33:53 +0800

    Stream

--- springboot10_my_precious/src/test/java/com/lwq/precious/p01_Stream/StreamTest.java
+++ springboot10_my_precious/src/test/java/com/lwq/precious/p01_Stream/StreamTest.java
@@ -18,10 +18,10 @@ import java.util.stream.Stream;
 class StreamTest {
 
     /**
-     * 分组groupingBy()
+     * 分组groupingBy(),返回map
      */
     @Test
-    public void name9998(){
+    public void name9998() {
         People build1 = People.builder().sex("男").name("唐三藏").build();
         People build2 = People.builder().sex("男").name("唐三彩").build();
         People build3 = People.builder().sex("女").name("老鼠精").build();
@@ -31,7 +31,7 @@ class StreamTest {
         pList.add(build2);
         pList.add(build3);
         pList.add(build4);
-        //注意这里是map
+        // 注意这里是map
         Map<String, List<People>> collect = pList.stream().collect(Collectors.groupingBy(People::getSex));
         System.out.println(collect);
 
@@ -46,9 +46,9 @@ class StreamTest {
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
-        list.stream().forEach(System.out::println);     //串行流
+        list.stream().forEach(System.out::println); // 串行流
         System.out.println("------------------------------------------");
-        list.stream().parallel().forEach(System.out::println);  //并行流
+        list.stream().parallel().forEach(System.out::println); // 并行流
     }
 
     /**
@@ -64,15 +64,33 @@ class StreamTest {
         peopleList.stream()
                 .map(o -> o.getAge())
                 .filter(o -> o > 17)
-                .map(o -> o + 10)           //这里这么做会出现频繁的拆箱装箱
+                .map(o -> o + 10) // 这里这么做会出现频繁的拆箱装箱
                 .forEach(System.out::println);
 
-        //这样优化一下即可
+        // 这样优化一下即可
         peopleList.stream()
                 .mapToInt(o -> o.getAge())
                 .filter(o -> o > 17)
                 .map(o -> o + 10)
                 .forEach(System.out::println);
+
+    }
+
+    // flatmap,例子都看完了,还是不会,感觉用处不多啊
+    @Test
+    public void name8() {
+
+        String[] words = new String[] { "Hello", "World" };
+        List<String[]> collect = Arrays.stream(words)
+                .map(word -> word.split(""))
+                .collect(Collectors.toList());
+        System.out.println(collect);
+
+        List<String> collect2 = Arrays.stream(words)
+                .map(word -> word.split(""))
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
+        System.out.println(collect2);
 
     }
 
@@ -90,6 +108,149 @@ class StreamTest {
         System.out.println(collect);
         List<String> collect1 = peopleList.stream().map(o -> "hehe").collect(Collectors.toList());
         System.out.println(collect1);
+    }
+
+    // 看不懂,有空再看
+    // reduce 可以传入一个初始值,依次将流中的元素与初始元素计算,计算结果再与后续元素计算
+    @Test
+    public void name10() {
+        ArrayList<People> peopleList = new ArrayList<People>();
+        peopleList.add(new People("李文强", 17, 6666));
+        peopleList.add(new People("孙文腾", 19, 5555));
+        peopleList.add(new People("屈百琛", 20, 4444));
+
+        Integer reduce = peopleList.stream()
+                .map(p -> p.getAge())
+                .reduce(0, (result, element) -> result + element);
+
+        System.out.println(reduce);
+    }
+
+    /**
+     * skip：跳过前n个
+     * findFirst()
+     */
+    @Test
+    public void name7() {
+        ArrayList<People> peopleList = new ArrayList<People>();
+        peopleList.add(new People("李文强", 17, 6666));
+        peopleList.add(new People("孙文腾", 19, 5555));
+        peopleList.add(new People("屈百琛", 20, 4444));
+
+        peopleList.stream()
+                .sorted((p1, p2) -> p2.getAge() - p1.getAge())
+                .skip(1)
+                .forEach(System.out::println);
+
+        Optional<People> findFirst = peopleList.stream().findFirst();
+        System.out.println(findFirst.get());
+    }
+
+    // anyMatch 任意一个元素符合条件,返回true
+    // allMatch 所有元素...
+    // noMatch 都不符合...
+    @Test
+    public void name9() {
+        ArrayList<People> peopleList = new ArrayList<People>();
+        peopleList.add(new People("李文强", 17, 6666));
+        peopleList.add(new People("孙文腾", 19, 5555));
+        peopleList.add(new People("屈百琛", 20, 4444));
+
+        Predicate<People> predicate1 = people -> people.getAge() > 1;
+        Predicate<People> predicate2 = people -> people.getSalary() > 4999;
+        boolean anyMatch = peopleList.stream().anyMatch(predicate1.and(predicate2));
+        boolean allMatch = peopleList.stream().allMatch(predicate1.and(predicate2));
+        boolean noneMatch = peopleList.stream().noneMatch(predicate1.and(predicate2));
+        System.out.println(anyMatch);
+        System.out.println(allMatch);
+        System.out.println(noneMatch);
+    }
+
+    // max & min & count（都是终结操作）
+    @Test
+    public void name5() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(4);
+        list.add(1);
+        list.add(5);
+        list.add(9);
+        Optional<Integer> max = list.stream().max(Integer::compareTo);
+        System.out.println(max.get());
+        Optional<Integer> min = list.stream().min(Integer::compareTo);
+        System.out.println(min.get());
+        long count = list.stream().count();
+        System.out.println(count);
+    }
+
+    // forEach是一个终结操作(不返回Stream就是终结操作了呗)
+    @Test
+    public void name666() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.stream().forEach(o -> method(o));
+        System.out.println(list);
+    }
+
+    private void method(String hehe) {
+        hehe = hehe + "z";
+        System.out.println(hehe);
+    }
+
+    // sorted & ::
+    // 需要去比较的类实现comparable接口或者直接lambda
+    @Test
+    public void name4() {
+        ArrayList<People> peopleList = new ArrayList<People>();
+        peopleList.add(new People("李文强", 17, 6666));
+        peopleList.add(new People("孙文腾", 19, 5555));
+        peopleList.add(new People("屈百琛", 20, 4444));
+
+        peopleList.stream().sorted((p1, p2) -> p2.getAge() - p1.getAge())
+                // .forEach(hehe->System.out.println(hehe));
+                .forEach(System.out::println);
+    }
+
+    // limit & distinct
+    @Test
+    public void name3() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(3);
+        list.add(1);
+        list.add(4);
+        list.add(1);
+        list.add(5);
+        list.add(9);
+
+        list.stream()
+                .distinct() // 去重
+                .limit(4) // 前四个
+                .forEach(number -> System.out.println(number));
+    }
+
+    // 双列集合获取Stream
+    @Test
+    public void name12() {
+        Map<String, String> myMap = new HashMap<String, String>();
+        myMap.put("瑟提", "叹为观止");
+        myMap.put("亚索", "狂风绝息斩");
+        myMap.put("Zed", "禁奥义-瞬狱影杀阵");
+        Set<Entry<String, String>> myEntrySet = myMap.entrySet();
+        myEntrySet.stream()
+                .filter(entry -> entry.getValue().length() > 4)
+                .forEach(entry -> System.out.println(entry));
+    }
+
+    // 数组获取Stream
+    @Test
+    public void name11() {
+        Integer[] arr = { 1, 2, 3, 4, 5 };
+        Stream<Integer> stream = Arrays.stream(arr);
+        stream.filter(num -> num < 4).forEach(num -> System.out.println(num));
     }
 
     // 单列集合获取Stream
@@ -115,177 +276,6 @@ class StreamTest {
         Predicate<People> predicate2 = people -> people.getSalary() > 4443;
         List<People> res = peopleList.stream().filter(predicate1.and(predicate2)).collect(Collectors.toList());
         System.out.println(res);
-    }
-
-    /**
-     * anyMatch & allMatch
-     */
-    @Test
-    public void nameaa() {
-
-        ArrayList<People> peopleList = new ArrayList<People>();
-        peopleList.add(new People("李文强", 17, 6666));
-        peopleList.add(new People("孙文腾", 19, 5555));
-        peopleList.add(new People("屈百琛", 20, 4444));
-
-        Predicate<People> predicate1 = people -> people.getAge() > 1;
-        Predicate<People> predicate2 = people -> people.getSalary() > 4444;
-        boolean anyMatch = peopleList.stream().anyMatch(predicate1.and(predicate2));
-        boolean allMatch = peopleList.stream().allMatch(predicate1.and(predicate2));
-        System.out.println(anyMatch);
-        System.out.println(allMatch);
-    }
-
-    // 数组获取Stream
-    @Test
-    public void name11() {
-        Integer[] arr = {1, 2, 3, 4, 5};
-        Stream<Integer> stream = Arrays.stream(arr);
-        stream.filter(num -> num < 4).forEach(num -> System.out.println(num));
-    }
-
-    // 双列集合获取Stream
-    @Test
-    public void name12() {
-        Map<String, String> myMap = new HashMap<String, String>();
-        myMap.put("瑟提", "叹为观止");
-        myMap.put("亚索", "狂风绝息斩");
-        myMap.put("Zed", "禁奥义-瞬狱影杀阵");
-        Set<Entry<String, String>> myEntrySet = myMap.entrySet();
-        myEntrySet.stream()
-                .filter(entry -> entry.getValue().length() > 4)
-                .forEach(entry -> System.out.println(entry));
-    }
-
-    // limit & distinct
-    @Test
-    public void name3() {
-        List<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(3);
-        list.add(1);
-        list.add(4);
-        list.add(1);
-        list.add(5);
-        list.add(9);
-
-        list.stream()
-                .distinct() // 去重
-                .limit(4) // 前四个
-                .forEach(number -> System.out.println(number));
-    }
-
-    // sorted & ::
-    // 需要去比较的类实现comparable接口或者直接lambda
-    @Test
-    public void name4() {
-        ArrayList<People> peopleList = new ArrayList<People>();
-        peopleList.add(new People("李文强", 17, 6666));
-        peopleList.add(new People("孙文腾", 19, 5555));
-        peopleList.add(new People("屈百琛", 20, 4444));
-
-        peopleList.stream().sorted((p1, p2) -> p2.getAge() - p1.getAge())
-                // .forEach(hehe->System.out.println(hehe));
-                .forEach(System.out::println);
-    }
-
-    // max & min & count（都是终结操作）
-    @Test
-    public void name5() {
-        List<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(1);
-        list.add(4);
-        list.add(1);
-        list.add(5);
-        list.add(9);
-        Optional<Integer> max = list.stream().max(Integer::compareTo);
-        System.out.println(max.get());
-        Optional<Integer> min = list.stream().min(Integer::compareTo);
-        System.out.println(min.get());
-        long count = list.stream().count();
-        System.out.println(count);
-    }
-
-    // forEach是一个终结操作
-    @Test
-    public void name666() {
-        List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.stream().forEach(o -> method(o));
-        System.out.println(list);
-    }
-
-    private void method(String hehe) {
-        hehe = hehe + "z";
-        System.out.println(hehe);
-    }
-
-    // map:对每一个值分别操作
-    // .collect(Collectors.toList()); 收集元素放进集合
-    @Test
-    public void name6() {
-        List<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(1);
-        list.add(4);
-        List<Integer> collect = list.stream()
-                .map(o -> o + 10)
-                .collect(Collectors.toList());
-        System.out.println(collect);
-    }
-
-    /// skip：跳过前n个
-    @Test
-    public void name7() {
-        ArrayList<People> peopleList = new ArrayList<People>();
-        peopleList.add(new People("李文强", 17, 6666));
-        peopleList.add(new People("孙文腾", 19, 5555));
-        peopleList.add(new People("屈百琛", 20, 4444));
-
-        peopleList.stream()
-                .sorted((p1, p2) -> p2.getAge() - p1.getAge())
-                .skip(1)
-                .forEach(System.out::println);
-    }
-
-    // flatmap将每一个元素都处理最后返回stream(具体在百度吧)
-    @Test
-    public void name8() {
-
-    }
-
-    // anyMatch 任意一个元素符合条件,返回true
-    // allMatch 所有元素...
-    // noMatch 都不符合...
-    @Test
-    public void name9() {
-        ArrayList<People> peopleList = new ArrayList<People>();
-        peopleList.add(new People("李文强", 17, 6666));
-        peopleList.add(new People("孙文腾", 19, 5555));
-        peopleList.add(new People("屈百琛", 20, 4444));
-
-        boolean anyMatch = peopleList.stream()
-                .anyMatch(p -> p.getSalary() > 6665);
-        System.out.println(anyMatch);
-    }
-
-    // 看不懂,有空再看
-    // reduce 可以传入一个初始值,依次将流中的元素与初始元素计算,计算结果再与后续元素计算
-    @Test
-    public void name10() {
-        ArrayList<People> peopleList = new ArrayList<People>();
-        peopleList.add(new People("李文强", 17, 6666));
-        peopleList.add(new People("孙文腾", 19, 5555));
-        peopleList.add(new People("屈百琛", 20, 4444));
-
-        Integer reduce = peopleList.stream()
-                .map(p -> p.getAge())
-                .reduce(0, (result, element) -> result + element);
-
-        System.out.println(reduce);
     }
 
 }
