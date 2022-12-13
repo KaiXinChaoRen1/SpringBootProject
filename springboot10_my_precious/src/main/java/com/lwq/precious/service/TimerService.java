a7aad1568b19f87fed651bcef5ae12180229cd49
lwqq <hehe@hehe.com>
2022-12-13 17:47:37 +0800

    1

--- springboot10_my_precious/src/main/java/com/lwq/precious/service/TimerService.java
+++ springboot10_my_precious/src/main/java/com/lwq/precious/service/TimerService.java
@@ -1,17 +1,13 @@
 package com.lwq.precious.service;
 
-
-
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,32 +15,43 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 指定时间运行的定时器
  */
+@EnableScheduling
 @Service
 @Slf4j
 public class TimerService {
 
+    /*
+     * 实现1:
+     * scheduleAtFixedRate
+     */
 
-    //@PostConstruct
-    public void hehe(){
-        //指定时间
+    // @PostConstruct
+    public void hehe() {
+        // 指定时间
         String dateTimeStr = "2022-12-08 19:21:15";
         LocalDateTime startTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        //LocaldateTime转换成Date对象
+        // LocaldateTime转换成Date对象
         Date from = Date.from(startTime.atZone(ZoneId.systemDefault()).toInstant());
 
-        Timer t=new Timer();
-        t.scheduleAtFixedRate(new TimerTask(){
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
 
             @Override
             public void run() {
-               log.info("我执行了现在是"+LocalDateTime.now().toString());
+                log.info("我执行了现在是" + LocalDateTime.now().toString());
             }
-    
-        },from,1000);
-    }
-    
 
-        
-    
+        }, from, 1000);
+    }
+
+    /**
+     * 实现2:
+     * Scheduled
+     */
+
+    // @Scheduled(cron="0/1 46 17 * * ?")
+    public void hehe2() {
+        log.info("我执行了现在是" + LocalDateTime.now().toString());
+    }
 
 }
