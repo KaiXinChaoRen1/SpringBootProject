7a8b6fe50620797c6ba8e75378edd18ab3f7e2d0
lwq <2065277594@qq.com>
2022-12-27 20:28:51 +0800

    1

---
+++ springboot10_my_precious/src/main/java/com/lwq/precious/controller/WordController.java
@@ -0,0 +1,65 @@
+package com.lwq.precious.controller;
+
+import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Slf4j
+@Api(tags = "导出word")
+@RestController
+@RequestMapping("/word")
+public class WordController {
+
+    @ApiOperation("word1")
+    @PostMapping("/word1")
+    public void exportWord(HttpServletResponse response) throws Exception {
+        OutputStream out = null;
+
+        //获取Word模板
+        InputStream ins = new FileInputStream("C:\\Users\\lwq\\Desktop\\a.xml");
+        //注册xdocreport实例并加载FreeMarker模板引擎
+        IXDocReport report = XDocReportRegistry.getRegistry().loadReport(ins, TemplateEngineKind.Freemarker);
+        //创建xdocreport上下文对象
+        IContext context;
+        context = report.createContext();
+        ArrayList<HashMap<String, String>> dataList = new ArrayList<>();
+        HashMap<String, String> stringstringHashMap = new HashMap<String, String>();
+        stringstringHashMap.put("haha","哈哈");
+        HashMap<String, String> stringstringHashMap2 = new HashMap<String, String>();
+        stringstringHashMap2.put("haha","哈哈2");
+        dataList.add(stringstringHashMap);
+        dataList.add(stringstringHashMap2);
+
+        context.put("hehe", "呵呵");
+        context.put("dataList", dataList);
+
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/msword");
+        response.setHeader("Content-Disposition", "attachment; filename=".concat(String.valueOf(URLEncoder.encode("美国重大时间.docx", "UTF-8"))));
+        out = response.getOutputStream();
+        report.process(context, out);
+        out.flush();
+        out.close();
+
+    }
+}
+
