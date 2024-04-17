5f1981cc928221e374659901b3892917585a277d
lwqq <hehe@hehe.com>
2024-04-17 11:40:38 +0800

    1

---
+++ springboot12_Hutool/src/main/java/com/lwq/hutool/RunApplication.java
@@ -0,0 +1,17 @@
+package com.lwq.hutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class RunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+
+    }
+
+}
