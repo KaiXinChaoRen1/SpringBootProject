a39d9d7e8a5fcc6c98668341220f33ad452bb26f
lwqq <hehe@hehe.com>
2025-01-06 11:28:59 +0800

    1

--- 2024.11.11/jk1_all/src/test/java/com/lwq/jk1/j01_随心所欲/Passion.java
+++ 2024.11.11/jk1_all/src/test/java/com/lwq/jk1/j01_随心所欲/Passion.java
@@ -1,5 +1,6 @@
 package com.lwq.jk1.j01_随心所欲;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class Passion {
+
+    @Test
+    public void a2() {
+        long t1 = System.currentTimeMillis();
+        ArrayList<Double> arrayList = new ArrayList<Double>();
+        for (int i = 0; i < 10000000; i++) {
+            arrayList.add(0.2);
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println(t2 - t1);
+    }
+
+    @Test
+    public void a1() {
+
+        long t3 = System.currentTimeMillis();
+        ArrayList<Double> list = new ArrayList<Double>(10000000);
+        for (int i = 0; i < 10000000; i++) {
+            list.add(0.2);
+        }
+        long t4 = System.currentTimeMillis();
+        System.out.println(t4 - t3);
+
+    }
 
     @Test
     public void name4() {
